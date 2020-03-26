980efe6153fa1943a6e720675202551366a34550
shubhammahawar <shubham.mahawar1001@gmail.com>
2020-03-27 02:57:07 +0530

    Update segment tree.cpp

--- segment tree.cpp
+++ segment tree.cpp
@@ -1,3 +1,5 @@
+
+  
 #include<bits/stdc++.h>
 #include <iostream>
 using namespace std;
@@ -16,7 +18,6 @@ void segmenttree(int si,int ss,int se)  ////// SEGMENT TREE IMPLEMENTATION
     segmenttree(si*2+1,md+1,se);       // move to right subtree
     
     st[si]=min(st[si*2],st[si*2+1]);   // store the min value youe got from left and right subtree
-     
 }
 
 int query(int si,int ss,int se,int qs,int qe)   // function to perform query on range  qs- start query qe-end query
@@ -30,14 +31,33 @@ int query(int si,int ss,int se,int qs,int qe)   // function to perform query on 
      
      int md=(ss+se)/2;
      
-     int l=query(si,ss,md,qs,qe);   // move to the left part and find min
-     int r=query(si,md+1,se,qs,qe);    // move to right part and find min
+     int l=query(si*2,ss,md,qs,qe);   // move to the left part and find min
+     int r=query(si*2+1,md+1,se,qs,qe);    // move to right part and find min
      
      return min(l,r);    // return min (left,right) subtree
      
      
 }
     
+void update(int si,int ss,int se,int qi)
+{
+    if(ss==se)
+    {
+        st[si]=a[qi];
+        return;
+    }
+  
+    int md=(ss+se)/2;
+   
+    if(qi<=md)
+    update(2*si,ss,md,qi);
+    
+    else
+    update(2*si+1,md+1,se,qi);
+    
+    st[si]=min(st[2*si],st[2*si+1]);
+      
+}
 
 
 
@@ -56,10 +76,25 @@ segmenttree(1,1,n);   // building the segment tree
 for(i=0;i<q;i++)
 {
     int qs,qe;
-    cin>>qs>>qe;
+    char ch;
+    cin>>ch;
+   
     
+    if(ch=='Q')   // If its a query to find in a range
+    {
+         cin>>qs>>qe;
     int ans=query(1,1,n,qs,qe);   // for every query calling the function
     cout<<ans<<endl;
+    }
+    else if(ch=='U')  // If its a updation to update at particular position
+    {
+       int pos,val;
+       cin>>pos>>val;
+       a[pos]=val;
+     
+        update(1,1,n,pos);
+    }
+    
 }
 
 	return 0;
